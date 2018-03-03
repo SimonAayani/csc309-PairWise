2ee20b0de8baf608f40961e3c544a3fd5fbeba67
ahurka <hurka.alex@gmail.com>
2018-03-02 20:39:26 -0500

    Save progress for transfer
    Uploading to access on another computer. Weekdays have been enforced as
    an enum in the CourseTerm table; further efforts have been made into
    uploading parsed JSON files from Cobalt API to the database.

--- PairWise/migrate_courses.py
+++ PairWise/migrate_courses.py
@@ -1,4 +1,6 @@
 from requests import get
+from PairWise.models.courses import Course, CourseTerm, TimeSection, CourseSection
+import datetime
 
 
 def load_course_data():
@@ -23,5 +25,53 @@ def load_course_data():
     return combined
 
 
+def _convert_term(c_code, term_str):
+    if 'Summer' in term_str:
+        term_code = 'S' + c_code[-1]
+    else:
+        term_code = c_code[-1]
+
+    return term_code
+
+
+def _convert_time_of_day(time_in_seconds):
+    time_obj = datetime.time(hour=time_in_seconds % 3600)
+    return time_obj
+
+
+def _convert_weekday(weekday):
+    return {
+        'MONDAY': 'M',
+        'TUESDAY': 'T',
+        'WEDNESDAY': 'W',
+        'THURSDAY': 'R',
+        'FRIDAY': 'F',
+        'SATURDAY': 'A',
+        'SUNDAY': 'S',
+    }.get(weekday, None)
+
+
+def migrate():
+    data = load_course_data()
+    for course in data:
+        # Require course code, name, term, times, section names per section
+        course_model = Course.objects.get_or_create(course_code=course['code'], name=course['name'])[0]
+
+        section_term = _convert_term(course['code'], course['term'])
+        new_course_term = CourseTerm.objects.get_or_create(course_id=course_model.course_id, term=section_term)[0]
+
+        for section in course['meeting_sections']:
+            section_code = section['code']
+            new_section = CourseSection.objects.get_or_create(term_id=new_course_term.term_id, section_name=section_code)[0]
+
+            for session in section['times']:
+                session_day = _convert_weekday(session['day'])
+                start_time = _convert_time_of_day(session['start'])
+                end_time = _convert_time_of_day(session['end'])
+
+                time_slot = TimeSection.objects.get_or_create(day=session_day, start_time=start_time, end_time=end_time)[0]
+                new_section.times.add(time_slot)
+
+
 if __name__ == '__main__':
-    load_course_data()
+    migrate()
