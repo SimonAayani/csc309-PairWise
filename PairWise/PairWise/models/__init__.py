be3d43b0fb3e2006011b9a00883e849e2118f891
ahurka <hurka.alex@gmail.com>
2018-03-04 00:20:26 -0500

    Add notifications schema

--- PairWise/PairWise/models/__init__.py
+++ PairWise/PairWise/models/__init__.py
@@ -2,21 +2,5 @@ from .users import Profile, Group
 from .courses import Course, CourseOffering, CourseSection, TimeSection, Term
 from .searches import SearchEntry, UserSearchEntry, GroupSearchEntry, Search, SearchResultsCache
 from .data_tags import DataTag, SkillTag, LanguageTag, ConceptTag, FrameworkTag, LocationTag
+from .messages import Notification, NewNotification
 
-
-
-# You Need in the Database:
-
-# User Accounts (LoginView)
-# Profile Data (Name, Courses, Location, Bio, Skills, Picture)
-# -- Skills Tags (extend Languages, Frameworks, Concepts)
-# -- Location Options
-
-# Notifications per user?
-#  -- New notifications?
-#  Courses
-
-# Search Criteria
-# -- Course, Purpose (custom), Capacity, Skillsets (references skill tags), Description
-# Search Results
-# -- New Results (user for updates)
