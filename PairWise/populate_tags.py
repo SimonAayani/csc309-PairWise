688aaf1db45d34f6bbcd1406758350ad0b962e90
hurkaale <alex.hurka@mail.utoronto.ca>
2018-03-03 22:36:29 -0500

    Test tag upload to database

---
+++ PairWise/populate_tags.py
@@ -0,0 +1,101 @@
+from PairWise.models.data_tags import LanguageTag, ConceptTag, FrameworkTag, LocationTag
+from django.db.transaction import atomic
+
+language_options = [
+        'Assembly',
+        'Bash',
+        'C',
+        'C++',
+        'C#',
+        'Haskell',
+        'HTML',
+        'Lisp',
+        'Java',
+        'JavaScript',
+        'MATLAB',
+        'Pascal',
+        'Perl',
+        'PHP',
+        'Prolog',
+        'Python',
+        'R',
+        'Racket',
+        'Ruby',
+        'Scheme',
+        'SQL',
+        'Swift',
+        'Unity',
+        'Verilog',
+        'VHDL'
+    ]
+
+concept_options = [
+        'Algorithm Design',
+        'Artificial Intelligence',
+        'Backend Development',
+        'Compilers',
+        'Computer Graphics',
+        'Concurrency',
+        'Data Structures',
+        'Database Systems',
+        'Frontend Development',
+        'Functional Programming',
+        'Hardware',
+        'Machine Learning',
+        'Object-Oriented Design',
+        'Software Process',
+        'UI/UX Design',
+    ]
+
+framework_options = [
+        'Angular',
+        'Bootstrap',
+        'Django',
+        'Flask',
+        'JQuery',
+        'MongoDB',
+        'MySQL',
+        '.NET',
+        'Node',
+        'Oracle',
+        'PostgreSQL',
+        'Pyramid',
+        'React',
+        'Rails',
+    ]
+
+location_options = [
+        'BA3200',
+        'Brampton',
+        'Downtown',
+        'East York',
+        'Etobikoke',
+        'Markham',
+        'Mississauga',
+        'North York',
+        'Richmond Hill',
+        'Scarborough',
+        'Vaughan',
+        'York',
+    ]
+
+
+def load_tags(data, dest_class):
+    if type(data[0]) == str:
+        for i in range(len(data)):
+            data[i] = dest_class(tag_text=data[i])
+
+    with atomic():
+        for datapoint in data:
+            datapoint.save()
+
+
+if __name__ == '__main__':
+    load_tags(language_options, LanguageTag)
+    print('Finished Languages')
+    load_tags(concept_options, ConceptTag)
+    print('Finished Concepts')
+    load_tags(framework_options, FrameworkTag)
+    print('Finished Frameworks')
+    load_tags(location_options, LocationTag)
+    print('Finished Locations')
