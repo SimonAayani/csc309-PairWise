a1b14819cffca2b3f694c2f164d313f03f218dee
ahurka <hurka.alex@gmail.com>
2018-03-03 23:40:23 -0500

    Refactor searches schema
    Created a more robust schema for searches, which uses table inheritance
    to present a better interface. Now, searches are directly linked to
    groups and users, instead of being indirectly linked to groups through
    an arbitrarily selected leader.
    
    Other modules have not been updated to work with the new schema; in
    particular, search_ops.py and group_ops.py no longer use the correct
    model classes and thus do not perform their functions.

--- PairWise/PairWise/models/__init__.py
+++ PairWise/PairWise/models/__init__.py
@@ -1,3 +1,22 @@
-from .users import Enrolment, Group
+from .users import Profile, Group
 from .courses import Course, CourseSection, TimeSection
-from .searches import Searching, Results
+from .searches import SearchEntry, UserSearchEntry, GroupSearchEntry, Search, SearchResultsCache
+from .data_tags import DataTag, SkillTag, LanguageTag, ConceptTag, FrameworkTag, LocationTag
+
+
+
+# You Need in the Database:
+
+# User Accounts (LoginView)
+# Profile Data (Name, Courses, Location, Bio, Skills, Picture)
+# -- Skills Tags (extend Languages, Frameworks, Concepts)
+# -- Location Options
+
+# Notifications per user?
+#  -- New notifications?
+#  Courses
+
+# Search Criteria
+# -- Course, Purpose (custom), Capacity, Skillsets (references skill tags), Description
+# Search Results
+# -- New Results (user for updates)
--- PairWise/PairWise/models/searches.py
+++ PairWise/PairWise/models/searches.py
@@ -1,14 +1,31 @@
 from django.db import models
-from PairWise.models.users import User
-from PairWise.models.courses import CourseSection
+from PairWise.models.users import User, Group
+from PairWise.models.courses import CourseTerm
+from PairWise.models.data_tags import SkillTag
 
 
-class Searching(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    section_id = models.ForeignKey(CourseSection, on_delete=models.CASCADE)
+class SearchEntry(models.Model):
+    category = models.ForeignKey(CourseTerm, on_delete=models.CASCADE)
+    subhead = models.CharField(max_length=255)
+    capacity = models.PositiveSmallIntegerField()
+    description = models.TextField()
+    desired_fields = models.ManyToManyField(SkillTag)
+
+
+class UserSearchEntry(SearchEntry):
+    host = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class GroupSearchEntry(SearchEntry):
+    host = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+
+class Search(models.Model):
+    id = models.AutoField(primary_key=True)
+    searcher = models.ForeignKey(SearchEntry, on_delete=models.CASCADE)
     active_search = models.BooleanField()
 
 
-class Results(models.Model):
-    searcher = models.ForeignKey(User, on_delete=models.CASCADE)
-    result = models.ForeignKey(User, related_name='found_id', on_delete=models.CASCADE)
+class SearchResultsCache(models.Model):
+    searcher = models.ForeignKey(Search, on_delete=models.CASCADE)
+    result = models.ForeignKey(Search, related_name='found', on_delete=models.CASCADE)
