9c00c38647650dba1e868a8b7d34da41814afbf9
Deanne <deanne.madulid@mail.utoronto.ca>
2018-03-09 18:45:19 -0500

    Created basic 'view profile page'

--- pairwise/src/App.js
+++ pairwise/src/App.js
@@ -1,31 +1,44 @@
 import React from 'react'
 import 'semantic-ui-css/semantic.min.css'
-import { Card, Image, Icon, Divider, Container } from 'semantic-ui-react'
-/*import default_pic from './no-img.jpg';*/
+import { Image, Icon, Divider, Container, Header } from 'semantic-ui-react'
 const default_pic = 'https://chipinworld.com/assets1/images/about-image/no-image.jpg'
 
-const ProfileCard = () => (
-  <Card raised>
-  <Card.Content>
-    <Container textAlign='center'>
-    <Image src={default_pic} size='small' circular bordered/>
-    </Container>
-    <Divider hidden />
-      <Card.Header textAlign='center'>FirstName LastName</Card.Header>
-      <Card.Meta>
-        <Container textAlign='center'> 
-          <Icon name='marker'/> Location
-        </Container>
-      </Card.Meta>
-      <Card.Description>This is the user's bio. Just adding a little text to make
-      it longer for now.</Card.Description>
-    </Card.Content>
-    <Card.Content>
-      <a>
-        View Full Profile
-      </a>
-    </Card.Content>
-  </Card>
+const OtherUserProfile = () => (
+<Container>
+      <Image src={default_pic} size='small' circular bordered centered/>
+      <Divider hidden />
+      <Container textAlign='center'>
+        <Header name='user_name' as='h1' textAlign='center'>
+          FirstName LastName
+        </Header>
+        <Icon name='marker'/> Location
+      </Container>
+      <Divider hidden />
+    <p name='bio'>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </p>
+    <Header as='h2'>Skills</Header>
+      {skills}
+    <Header as='h2'>Courses Currently Taking</Header>
+      {courses}
+    <p name='courses'>
+    </p>
+  </Container>
 )
 
-export default ProfileCard
+var skillsList = ["SQL", "JavaScript", "C", "Python", "Java", "React", "Node.js", "C++"]
+var skills = CreateSkillsText(skillsList)
+
+var coursesList = ["CSC369", "CSC301", "CSC309"]
+var courses = CreateCoursesText(coursesList)
+
+
+function CreateSkillsText(skillsList) {
+  return skillsList.join(", ")
+}
+
+function CreateCoursesText(coursesList) {
+  return coursesList.join(", ")
+}
+
+export default OtherUserProfile
