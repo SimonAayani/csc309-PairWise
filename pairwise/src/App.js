b6fba0ffa47647aef62aee15425c3578deadfa57
Deanne <deanne.madulid@mail.utoronto.ca>
2018-03-09 16:57:54 -0500

    Fixed formatting of Profile Card

--- pairwise/src/App.js
+++ pairwise/src/App.js
@@ -1,20 +1,27 @@
 import React from 'react'
 import 'semantic-ui-css/semantic.min.css'
-import { Card, Image, Icon } from 'semantic-ui-react'
+import { Card, Image, Icon, Divider, Container } from 'semantic-ui-react'
 /*import default_pic from './no-img.jpg';*/
+const default_pic = 'https://chipinworld.com/assets1/images/about-image/no-image.jpg'
 
 const ProfileCard = () => (
-  <Card>
-    <Image src='https://chipinworld.com/assets1/images/about-image/no-image.jpg' /> {/* Need to fix*/}
-    <Card.Content>
-      <Card.Header>FirstName LastName</Card.Header>
-      <Card.Meta>Location</Card.Meta>
+  <Card raised>
+  <Card.Content>
+    <Container textAlign='center'>
+    <Image src={default_pic} size='small' circular bordered/>
+    </Container>
+    <Divider hidden />
+      <Card.Header textAlign='center'>FirstName LastName</Card.Header>
+      <Card.Meta>
+        <Container textAlign='center'> 
+          <Icon name='marker'/> Location
+        </Container>
+      </Card.Meta>
       <Card.Description>This is the user's bio. Just adding a little text to make
       it longer for now.</Card.Description>
     </Card.Content>
-    <Card.Content fullProfile>
+    <Card.Content>
       <a>
-        <Icon name='fullProfile' />
         View Full Profile
       </a>
     </Card.Content>
