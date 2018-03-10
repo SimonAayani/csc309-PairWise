1e0d31093ef73013a4fec94d8e58a4760a97c72f
Salt Li <nacl1457@hotmail.com>
2018-03-10 16:37:33 -0500

    Setting up

---
+++ frontend/src/components/Profile.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import {button, tab, grid} from "semantic-ui-react";
+
+
+
+class Profile extends Component{
+	render() {
+    	return (
+    	)
+  	}
+
+}
+
+export default Profile
