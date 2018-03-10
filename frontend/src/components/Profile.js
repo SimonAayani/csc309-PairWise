1c5995ef7f2d65d2233767acacba7f93b6f5c9bd
Salt Li <nacl1457@hotmail.com>
2018-03-10 18:02:28 -0500

    Done tonight

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,14 +1,38 @@
 import React, { Component } from "react";
-import {button, tab, grid} from "semantic-ui-react";
+import { Button, Grid, Tab, Image } from "semantic-ui-react";
+import avatar from '../avatar.png'
 
 
 
-class Profile extends Component{
+export default class Profile extends Component{
+	
+
+
 	render() {
+
+
+		const panes = [
+  		{ menuItem: 'Profile', 
+  			pane: (
+  				<Tab.Pane key = "1">
+  				<Image src = {avatar} size = "small" />
+      			</Tab.Pane>
+
+  		) },
+  		{ menuItem: 'Security', pane: { key: 'tab1', content: 'This is massive tab', size: 'massive' } },
+  		{ menuItem: 'Notifiction', pane: { key: 'tab1', content: 'This is massive tab', size: 'massive' } },
+  		{ menuItem: 'Social', pane: { key: 'tab1', content: 'This is massive tab', size: 'massive' } },
+		]
+
     	return (
+    		<Grid textAlign='center'>
+    			<Grid.Column width={10}>
+    			
+  				<Tab menu={{ fluid: true, vertical: true, tabular: 'right'}} panes={panes} renderActiveOnly={false}/>
+
+    			</Grid.Column>
+    		</Grid>
     	)
   	}
 
 }
-
-export default Profile
