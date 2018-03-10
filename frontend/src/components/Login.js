b67b4722c3beb8af0045a483cb81f929867b61af
Evan Wallace <evan.wallace@mail.utoronto.ca>
2018-03-09 19:09:43 -0500

    create dynamic Login/Logout link

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,14 +1,30 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 
 class Login extends Component {
-    render() {
-        return (
-            <div className="login">
-            <h2>Login</h2>
-            <button onClick={this.props.handleLogin}>Log In</button>
-            </div>
-        )
+  constructor(props) {
+    super(props);
+
+    this.redirectHome = this.redirectHome.bind(this);
+  }
+
+  redirectHome() {
+    this.props.history.push("/");
+  }
+
+  render() {
+    const handler = (e) => {
+      this.props.handleLogin(e);
+      this.redirectHome();
     }
+
+    return (
+      <div className="login">
+        <h2>Login</h2>
+        <button onClick={handler}>Log In</button>
+      </div>
+    )
+  }
 }
 
-export default Login;
+export default withRouter(Login);
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, NavLink, BrowserRouter, Redirect} from "react-router-dom";
+import { Route, NavLink, BrowserRouter } from "react-router-dom";
 
 import Dashboard from './Dashboard';
 import Login from './Login';
@@ -8,41 +8,66 @@ import Splash from './Splash';
 class Main extends Component {
   constructor(props) {
     super(props);
+
+    // for now, we'll model user authentication with a boolean state variable
     this.state = {isLoggedIn: false};
 
+    // bindings
     this.handleLogin = this.handleLogin.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
   }
 
+  // functions for toggling Login/Logout
   handleLogin(e) {
     e.preventDefault();
-    this.setState({
-      isLoggedIn: true
-    });
-    console.log("handle login called");
+    this.setState({isLoggedIn: true});
+  }
+
+  handleLogout(e) {
+    e.preventDefault();
+    this.setState({isLoggedIn: false});
   }
 
   render() {
-    const isLoggedIn = this.state.isLoggedIn;
     return (
       <BrowserRouter>
         <div className="main">
           <div className="header">
-          <h1>PairWise</h1>
-          <ul>
-            <li><NavLink exact to="/">Home</NavLink></li>
-            <li><NavLink to="/login">Login</NavLink></li>
-          </ul>
-        </div>
-          <div className="content">
-            <Route exact path="/" render={() => isLoggedIn ? <Dashboard /> : <Splash />} />
-            <Route path="/login" render={() => <Login handleLogin={this.handleLogin} />} />
-           <Route path="/splash" component={Splash}/>
+            <h1>PairWise</h1>
+            <ul>
+              <li><NavLink exact to="/">Home</NavLink></li>
+              <li><LoginLink
+                    isLoggedIn={this.state.isLoggedIn}
+                    handleLogin={this.handleLogin}
+                    handleLogout={this.handleLogout}
+                  /></li>
+            </ul>
           </div>
-
+          <div className="content">
+            <Route exact path="/" render={() => this.state.isLoggedIn ? <Dashboard /> : <Splash />} />
+            <Route path="/login" render={() =>
+                <Login handleLogin={this.handleLogin} />
+            } />
+          <Route path="/splash" component={Splash}/>
+          <Route path="/dashboard" component={Dashboard}/>
+          <Route path="/logout" component={Splash} />
         </div>
-      </BrowserRouter>
+
+      </div>
+    </BrowserRouter>
     )
   }
 }
+
+class LoginLink extends Component {
+  render() {
+    if (this.props.isLoggedIn) {
+      return(<NavLink to="/logout" onClick={this.props.handleLogout}>Log Out</NavLink>)
+    } else {
+      return(<NavLink to="/login">Log In</NavLink>)
+    }
+  }
+}
+
 
 export default Main;
