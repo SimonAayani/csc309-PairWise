84292c1eb3d971294296951c1dbfa4b87f0ebeac
cwang0802 <shuqiao.wang@mail.utoronto.ca>
2018-03-11 14:51:11 -0400

    fix overflow issue

--- frontend/src/components/SearchResults.js
+++ frontend/src/components/SearchResults.js
@@ -2,69 +2,74 @@ import React, { Component } from 'react';
 import { Button, Card, Image } from 'semantic-ui-react'
 import logo from '../logo.svg'
 
-const SearchResults = () => (
-  <Card.Group>
-    <Card>
-      <Card.Content>
-        <Image floated='right' size='mini' src={logo} />
-        <Card.Header>
-          Steve Sanders
-        </Card.Header>
-        <Card.Meta>
-          Friends of Elliot
-        </Card.Meta>
-        <Card.Description>
-          Steve wants to add you to the group <strong>best friends</strong>
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <div className='ui two buttons'>
-          <Button basic color='green'>Approve</Button>
-          <Button basic color='red'>Decline</Button>
-        </div>
-      </Card.Content>
-    </Card>
-    <Card>
-      <Card.Content>
-        <Image floated='right' size='mini' src={logo} />
-        <Card.Header>
-          Molly Thomas
-        </Card.Header>
-        <Card.Meta>
-          New User
-        </Card.Meta>
-        <Card.Description>
-          Molly wants to add you to the group <strong>musicians</strong>
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <div className='ui two buttons'>
-          <Button basic color='green'>Approve</Button>
-          <Button basic color='red'>Decline</Button>
-        </div>
-      </Card.Content>
-    </Card>
-    <Card>
-      <Card.Content>
-        <Image floated='right' size='mini' src={logo} />
-        <Card.Header>
-          Jenny Lawrence
-        </Card.Header>
-        <Card.Meta>
-          New User
-        </Card.Meta>
-        <Card.Description>
-          Jenny requested permission to view your contact details
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <div className='ui two buttons'>
-          <Button basic color='green'>Approve</Button>
-          <Button basic color='red'>Decline</Button>
-        </div>
-      </Card.Content>
-    </Card>
-  </Card.Group>
-)
+export default class SearchResults extends Component {
 
-export default SearchResults
+  render() {
+    return (
+      <Card.Group>
+        <Card fluid>
+          <Card.Content>
+            <Image floated='right' size='mini' src={logo} />
+            <Card.Header>
+              Steve Sanders
+            </Card.Header>
+            <Card.Meta>
+              Friends of Elliot
+            </Card.Meta>
+            <Card.Description>
+              Steve wants to add you to the group <strong>best friends</strong>
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <div className='ui two buttons'>
+              <Button basic color='green'>View Profile</Button>
+              <Button basic color='green'>Send Message</Button>
+            </div>
+          </Card.Content>
+        </Card>
+
+        <Card fluid>
+          <Card.Content>
+            <Image floated='right' size='mini' src={logo} />
+            <Card.Header>
+              Molly Thomas
+            </Card.Header>
+            <Card.Meta>
+              New User
+            </Card.Meta>
+            <Card.Description>
+              Molly wants to add you to the group <strong>musicians</strong>
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <div className='ui two buttons'>
+              <Button basic color='green'>Approve</Button>
+              <Button basic color='red'>Decline</Button>
+            </div>
+          </Card.Content>
+        </Card>
+
+        <Card fluid>
+          <Card.Content>
+            <Image floated='right' size='mini' src={logo} />
+            <Card.Header>
+              Jenny Lawrence
+            </Card.Header>
+            <Card.Meta>
+              New User
+            </Card.Meta>
+            <Card.Description>
+              Jenny requested permission to view your contact details
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <div className='ui two buttons'>
+              <Button basic color='green'>Approve</Button>
+              <Button basic color='red'>Decline</Button>
+            </div>
+          </Card.Content>
+        </Card>
+      </Card.Group>
+    )
+  }
+}
