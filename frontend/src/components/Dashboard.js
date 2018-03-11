725290ad1944023cd8f446fbe9bc417209ab27d0
Priyanka <priyanka.narasimhan.mail@utoronto.ca>
2018-03-11 13:33:42 -0400

    Navigation bar fixed

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -3,19 +3,23 @@ import { Grid, Segment } from 'semantic-ui-react'
 import Sidebar from './Sidebar.js'
 import Notifications from './Notifications.js'
 import './Dashboard.css'
+import NavBar from './NavBar.js';
 
 class Dashboard extends Component {
     render() {
         return (
             <div className="App">
+
 		        <Grid centered columns="two">
 		          <Grid.Row centered columns="four">
-		            <Grid.Column width={3} left floated>
+		            <Grid.Column width={4} left floated>
+		            
 		              <Segment id="side">
 		              	<Sidebar />
 		              </Segment>
 		            </Grid.Column>
-		            <Grid.Column width={8} right floated>
+		            <Grid.Column width={1}></Grid.Column>
+		            <Grid.Column width={8} left floated>
 		              <Segment padded id="contents">
 		              	<Notifications />
 		              </Segment>
